4918832a4d0679a91c669971c1c54d990c999ff6
katebennu <kate.dorrer@gmail.com>
2017-11-09 00:52:04 +0200

    add api service to docker compose

--- finu/api.py
+++ finu/api.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 
 class AllRates(Resource):
+
     def get(self):
         return json.load(open('ratios.json', 'r'))
 
@@ -14,4 +17,4 @@ class AllRates(Resource):
 api.add_resource(AllRates, '/all-rates/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
