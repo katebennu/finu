f650f5c891bfc78d8d957e7c0a50b78819ced959
katebennu <kate.dorrer@gmail.com>
2017-11-11 23:24:30 +0200

    pass data to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
 
-const App = ({data}: {data: object}) => (
+type Rates = {
+    company: string,
+    year: string
+}
+
+const Tile = ({value}: { value: Rates}) => (
+    <div>{value.company} {value.year}</div>
+);
+
+const App = ({data}: {data: Rates[]}) => (
     <div className="App">
         <header className="App-header">
             <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-            {JSON.stringify(data)}
+            {/*{JSON.stringify(data)}*/}
+            {data.map(statement => <Tile key={statement.company} value={statement}/>)}
         </p>
     </div>
 );
--- frontend/src/utils/fetchRates.ts
+++ frontend/src/utils/fetchRates.ts
@@ -1,7 +1,7 @@
 export default async function getRates(): Promise<any> {
     try {
         const response = await fetch('http://localhost:5000/all-rates/');
-        return await response.blob();
+        return await response.json();
     } catch (e) {
         console.log("Error!!!");
     }
