1a20840aba4879c12b7a2d132dacd58bbf6d72d3
katebennu <kate.dorrer@gmail.com>
2017-11-16 23:47:31 +0200

    select all/unsellect all

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,62 +1,70 @@
 import * as React from 'react';
-import { Set } from 'immutable';
+import {Set} from 'immutable';
 
 const Button = ({
-    value,
-    onClick,
-    isSelected
-}:{
+                    value,
+                    onClick,
+                    isSelected
+                }: {
     value: string,
     onClick: () => void,
     isSelected: boolean
 }) => (
     <button style={{
-            display: 'inline-block',
-            padding: '5px',
-            color: isSelected ? 'red' : 'black'
-        }}
-        key={value}
-        onClick={(e) => onClick()}
+        display: 'inline-block',
+        padding: '5px',
+        color: isSelected ? 'red' : 'black'
+    }}
+            key={value}
+            onClick={(e) => onClick()}
     >
         {value}
     </button>
 );
 
 const SelectableOptions = <ItemType extends {}>({
-    options,
-    selectedOptions,
-    selectedOptionsOnChange,
-    stringify
-}:{
+                                                    options,
+                                                    selectedOptions,
+                                                    selectedOptionsOnChange,
+                                                    stringify
+                                                }: {
     options: ItemType[],
     selectedOptions: ItemType[],
     selectedOptionsOnChange: (selectedOptions: ItemType[]) => void,
     stringify: (item: ItemType) => string
-}) => (<div>
-    {options.map(option => (
-        <Button
-            value={stringify(option)}
-            onClick={() => {
-                const selection = Set(selectedOptions);
-                if (selection.contains(option)) {
-                    selectedOptionsOnChange(selection.remove(option).toArray());
-                } else {
-                    selectedOptionsOnChange(selection.add(option).toArray());
-                }
-            }}
-            isSelected={selectedOptions.indexOf(option) >= 0}
-        />
-    ))}
-</div>);
+}) => (
+    <div>
+        {options.map(option => (
+            <Button
+                value={stringify(option)}
+                onClick={() => {
+                    const selection = Set(selectedOptions);
+                    if (selection.contains(option)) {
+                        selectedOptionsOnChange(selection.remove(option).toArray());
+                    } else {
+                        selectedOptionsOnChange(selection.add(option).toArray());
+                    }
+                }}
+                isSelected={selectedOptions.indexOf(option) >= 0}
+            />
+        ))}
+        <br />
+        <Button value={selectedOptions.length < options.length ? 'select all' : 'unselect all'}
+                onClick={() => {
+                    selectedOptions.length < options.length ?
+                        selectedOptionsOnChange(options) : selectedOptionsOnChange([]);
+                }}
+                isSelected={false}/>
+    </div>);
 
 const Menu = ({
-    companies,
-    selectedCompanies,
-    selectedCompaniesOnChange,
-    years,
-    selectedYears,
-    selectedYearsOnChange
-}: {
+                  companies,
+                  selectedCompanies,
+                  selectedCompaniesOnChange,
+                  years,
+                  selectedYears,
+                  selectedYearsOnChange
+              }: {
     companies: Company[],
     selectedCompanies: Company[],
     selectedCompaniesOnChange: (selectedCompanies: Company[]) => void,
@@ -80,12 +88,12 @@ const Menu = ({
         <div style={{
             margin: '20px'
         }}>
-        <SelectableOptions
-            options={years}
-            selectedOptions={selectedYears}
-            selectedOptionsOnChange={selectedYearsOnChange}
-            stringify={year => year}
-        />
+            <SelectableOptions
+                options={years}
+                selectedOptions={selectedYears}
+                selectedOptionsOnChange={selectedYearsOnChange}
+                stringify={year => year}
+            />
         </div>
     </div>
 );
