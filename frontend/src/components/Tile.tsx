7c1b3815c1a8a944d7f13ab985ac54bf6ec0699c
katebennu <kate.dorrer@gmail.com>
2017-11-17 00:31:40 +0200

    display all ratios

--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -1,21 +1,34 @@
 import * as React from 'react';
 
-const Tile = ({value}: { value: StatementRatios}) => (
+const RatiosSubGroup = ({title, items}: { title: string, items: object }) => (
+    <div style={{
+            border: 'solid 1px gray'
+        }}>
+        <b>{title}</b>
+        <ul>
+            {Object.keys(items).map((item: string) => (
+                    <li>{item}: {items[item].toFixed(2)}</li>
+                )
+            )}
+        </ul>
+    </div>
+);
+
+const Tile = ({value}: { value: StatementRatios }) => (
     <div style={{
         display: 'inline-block',
         width: '30%',
         minWidth: '200px',
         maxWidth: '300px',
-        margin: '10px'
+        margin: '10px',
+        border: 'solid 2px black'
     }}>
         <div>{value.company} {value.year}</div>
-        <div>Liquidity:
-            <div>Current Ratio: {JSON.stringify(value.ratios.Liquidity.CurrentRatio)}</div>
+        <div>
+            {Object.keys(value.ratios).map((ratio: string) =>
+                <RatiosSubGroup title={ratio} items={value.ratios[ratio]}/>
+            )}
         </div>
-        <div>CapitalStructure:
-            <div>Debt to Equity: {value.ratios.CapitalStructure.DebtToEquity}</div>
-        </div>
-
     </div>
 );
 
