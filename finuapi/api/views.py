6346602c865e540c192abfa806c0eb0fd018eb2c
katebennu <kate.dorrer@gmail.com>
2017-11-29 22:29:03 +0200

    company views

--- finuapi/api/views.py
+++ finuapi/api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Company
 
 
 def index(request):
@@ -7,4 +7,15 @@ def index(request):
 
 
 def company(request):
-    return HttpResponse("Companies")
+    if request.method == 'GET':
+        ticker = request.GET.get('ticker')
+        name = Company.objects.get(ticker=ticker).name
+        return HttpResponse('GET success: ' + ticker + ' ' + name)
+    elif request.method == 'PUT':
+        ticker = request.GET.get('ticker')
+        name = request.GET.get('name')
+        print(ticker, name)
+        c = Company(ticker=ticker, name=name)
+        c.save()
+        return HttpResponse("PUT success: " + ticker + ' ' + name)
+
