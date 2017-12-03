1e24e35317034da8080e1cf7a17c2cf638d27c7d
katebennu <kate.dorrer@gmail.com>
2017-12-03 21:22:20 +0200

    clean

--- finu/rebuild.sh
+++
@@ -1,7 +0,0 @@
-
-#! bin/bash
-docker-compose build
-docker-compose up -d db
-docker-compose run --rm api /bin/bash -c "cd /opt/services/api/src && python -c  \'import database; database.init_db()\'"
-docker-compose up
-
